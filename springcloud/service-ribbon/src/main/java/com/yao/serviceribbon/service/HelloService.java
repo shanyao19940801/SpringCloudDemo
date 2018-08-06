ad87168a1cb9d11b6d0cb7e68fd3989043d9a6d8
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-08-06 22:48:07 +0800

    SpringCloud熔断器

--- springcloud/service-ribbon/src/main/java/com/yao/serviceribbon/service/HelloService.java
+++ springcloud/service-ribbon/src/main/java/com/yao/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.yao.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    //该注解对该方法创建了熔断器的功能，并指定了fallbackMethod熔断方法
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi, " + name + ",sorry,error!";
     }
 }
