789a03f916da76fd75e146bb958fd6225389671a
DESKTOP-GLCVVSF\shanyao <sqshanyao@163.com>
2018-08-06 22:38:22 +0800

    SpringCloud的断路器

--- springcloud/service-ribbon/src/main/java/com/yao/serviceribbon/controler/HelloControler.java
+++ springcloud/service-ribbon/src/main/java/com/yao/serviceribbon/controler/HelloControler.java
@@ -1,5 +1,6 @@
-package com.yao.serviceribbon;
+package com.yao.serviceribbon.controler;
 
+import com.yao.serviceribbon.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
